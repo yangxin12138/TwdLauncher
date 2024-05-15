07b41821b5e8fcab74056ddd89af52a50d1159fa
Mulgokizary-ever <45618055+yangxin12138@users.noreply.github.com>
2024-05-15 17:38:39 +0800

    2024.5.15:重新规划界面,左上角时间日期,右上角网络USB耳机状态

--- app/src/main/java/com/twd/launcher/MainActivity.java
+++ app/src/main/java/com/twd/launcher/MainActivity.java
@@ -3,14 +3,20 @@ package com.twd.launcher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -24,27 +30,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnFocusChangeListener {
 
     private final static String TAG = MainActivity.class.getSimpleName();
     private TextView tv_time;
     private TextView tv_day;
-    private ImageButton bt_hdmi;
-    private ImageButton bt_setting;
-    private ImageButton bt_androidCast;
-    private ImageButton bt_airdrop;
-    private ImageButton bt_video;
-    private ImageButton bt_music;
-    private ImageButton bt_pic;
-    private ImageButton bt_office;
-
-    private ImageButton bt_dlan;
-
-    private ImageButton bt_wired;
     private ImageView im_usb;
+    private ImageView im_wifi;
+    private ImageView im_headset;
 
     private Handler timeHandler = new Handler();
+    private boolean hasNetworkConnection ;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +52,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initView();
         //初始化时间
+        sharedPreferences = getSharedPreferences("NetworkConnected",Context.MODE_PRIVATE);
+        hasNetworkConnection = sharedPreferences.getBoolean("hasNetworkConnection",false);
         updateTimeRunnable.run();
     }
 
     private Runnable updateTimeRunnable = new Runnable() {
         @Override
         public void run() {
+
+            if (!hasNetworkConnection){
+                //检查网络连接状态
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if (networkInfo != null && networkInfo.isConnected()){
+                    // 如果设备已连接到网络，从网络获取时间和日期数据
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("hasNetworkConnection",true);
+                    editor.apply();
+                    hasNetworkConnection = true;
+                    // 获取当前时间和日期
+                    Calendar calendar = Calendar.getInstance();
+                    Date currentDate = calendar.getTime();
+                    // 设置日期的格式
+                    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    String formattedDate = dateFormat.format(currentDate);
+                    // 设置时间的格式
+                    DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+                    String formattedTime = timeFormat.format(currentDate);
+                    // 在TextView上更新日期和时间
+                    tv_day.setText(formattedDate);
+                    tv_time.setText(formattedTime);
+                    // 每隔一秒更新一次时间
+                    timeHandler.postDelayed(this, 1000);
+                } else {
+                    // 如果设备未连接到网络，设置时间为--:--，日期不显示
+                    tv_time.setText("--:--");
+                    tv_day.setText("");
+                    // 每隔一秒检查网络连接状态
+                    timeHandler.postDelayed(this, 1000);
+                    return;
+                }
+            }
             //获取当前时间和日期
             Calendar calendar = Calendar.getInstance();
             Date currentDate = calendar.getTime();
 
             //设置日期的格式
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd EEEE");
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd ");
             String formattedDate = dateFormat.format(currentDate);
 
             //设置时间的格式
@@ -83,108 +120,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onDestroy() {
         super.onDestroy();
         timeHandler.removeCallbacks(updateTimeRunnable);
-        unregisterReceiver(usbDeviceReceiver);
+        //unregisterReceiver(usbDeviceReceiver);
+        unregisterReceiver(customReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateTimeRunnable.run();
+
+        IntentFilter customFilter = new IntentFilter();
+        customFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        customFilter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        customFilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        customFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+        registerReceiver(customReceiver,customFilter);
     }
 
     private void initView(){
         tv_time = findViewById(R.id.tv_time);
         tv_day = findViewById(R.id.tv_day);
-        bt_hdmi = findViewById(R.id.imButton_hdmi);
-        bt_setting = findViewById(R.id.imButton_setting);
-        bt_androidCast = findViewById(R.id.imButton_androidcast);
-        bt_airdrop = findViewById(R.id.imButton_airdrop);
-        bt_video = findViewById(R.id.imButton_video);
-        bt_music = findViewById(R.id.imButton_music);
-        bt_pic = findViewById(R.id.imButton_pic);
-        bt_office = findViewById(R.id.imButton_office);
-        bt_dlan = findViewById(R.id.im_dlan);
-        bt_wired = findViewById(R.id.im_wired);
         im_usb = findViewById(R.id.im_usb);
+        im_wifi = findViewById(R.id.im_wifi);
+        im_headset = findViewById(R.id.im_headset);
 
-        bt_hdmi.setFocusable(true);
-        bt_hdmi.setFocusableInTouchMode(true);
-        bt_setting.setFocusable(true);
-        bt_setting.setFocusableInTouchMode(true);
-        bt_androidCast.setFocusable(true);
-        bt_androidCast.setFocusableInTouchMode(true);
-        bt_airdrop.setFocusable(true);
-        bt_airdrop.setFocusableInTouchMode(true);
-        bt_video.setFocusable(true);
-        bt_video.setFocusableInTouchMode(true);
-        bt_music.setFocusable(true);
-        bt_music.setFocusableInTouchMode(true);
-        bt_pic.setFocusable(true);
-        bt_pic.setFocusableInTouchMode(true);
-        bt_office.setFocusable(true);
-        bt_office.setFocusableInTouchMode(true);
-        bt_dlan.setFocusable(true);
-        bt_dlan.setFocusableInTouchMode(true);
-        bt_wired.setFocusable(true);
-        bt_wired.setFocusableInTouchMode(true);
-
-        bt_hdmi.setOnFocusChangeListener(this);
-        bt_setting.setOnFocusChangeListener(this);
-        bt_androidCast.setOnFocusChangeListener(this);
-        bt_airdrop.setOnFocusChangeListener(this);
-        bt_video.setOnFocusChangeListener(this);
-        bt_music.setOnFocusChangeListener(this);
-        bt_pic.setOnFocusChangeListener(this);
-        bt_office.setOnFocusChangeListener(this);
-        bt_dlan.setOnFocusChangeListener(this);
-        bt_wired.setOnFocusChangeListener(this);
-
-        bt_hdmi.setOnClickListener(this);
-        bt_setting.setOnClickListener(this);
-        bt_androidCast.setOnClickListener(this);
-        bt_airdrop.setOnClickListener(this);
-        bt_video.setOnClickListener(this);
-        bt_music.setOnClickListener(this);
-        bt_pic.setOnClickListener(this);
-        bt_office.setOnClickListener(this);
-        bt_dlan.setOnClickListener(this);
-        bt_wired.setOnClickListener(this);
-
-        //获取UsbManager的实例
-        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        IntentFilter filter = new IntentFilter();
+        im_usb.setVisibility(usbIsConnect() ? View.VISIBLE : View.GONE);
+        im_headset.setVisibility(headsetIsWired() ? View.VISIBLE : View.GONE);
+        /*IntentFilter filter = new IntentFilter();
         filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        registerReceiver(usbDeviceReceiver,filter);
+        registerReceiver(usbDeviceReceiver,filter);*/
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.imButton_hdmi){
-            Toast.makeText(this, "点击hdmi", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "------------onClick: bt_hdmi");
-        } else if (v.getId() == R.id.imButton_setting) {
-            Toast.makeText(this, "点击Setting", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "------------onClick:  bt_setting");
-        } else if (v.getId() == R.id.imButton_androidcast) {
-            Toast.makeText(this, "点击androidCast", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "------------onClick:  bt_androidCast");
-        } else if (v.getId() == R.id.imButton_airdrop) {
-            Toast.makeText(this, "点击airdrop", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "------------onClick:  bt_airdrop");
-        } else if (v.getId() == R.id.imButton_video) {
-            Toast.makeText(this, "点击video", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "------------onClick:  bt_video");
-        } else if (v.getId() == R.id.imButton_pic) {
-            Toast.makeText(this, "点击picture", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "-------------onClick:  bt_picture");
-        } else if (v.getId() == R.id.imButton_music) {
-            Toast.makeText(this, "点击music", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "-------------onClick:  bt_music");
-        } else if (v.getId() == R.id.imButton_office) {
-            Toast.makeText(this, "点击office", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "--------------onClick:  bt_office");
-        } else if (v.getId() == R.id.im_dlan) {
-            Toast.makeText(this, "点击dlan", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "--------------onClick:  bt_dlan");
-        } else if (v.getId() == R.id.im_wired) {
-            Toast.makeText(this, "点击wired", Toast.LENGTH_SHORT).show();
-            Log.i(TAG, "--------------onClick:  bt_wired");
-        }
     }
     @Override
     public void onFocusChange(View view, boolean hasFocus) {
@@ -211,24 +180,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)){
-                //USB设备已经插入
-                UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
                 //执行相应的逻辑
-                im_usb.setImageResource(R.drawable.icon_usb_normal);
+                im_usb.setImageResource(R.drawable.icon_usb);
                 Toast.makeText(context, "-----插入------", Toast.LENGTH_SHORT).show();
             } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-                //USB设备已经拔出
-                UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
                 //执行相应的逻辑
-                im_usb.setImageResource(R.drawable.icon_usb_remove);
-                Toast.makeText(context, "-----拔出------", Toast.LENGTH_SHORT).show();
-                try {
-                    Thread.sleep(2000);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
                 im_usb.setImageDrawable(null);
+                Toast.makeText(context, "-----拔出------", Toast.LENGTH_SHORT).show();
             }
         }
     };
+
+    private BroadcastReceiver customReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action != null){
+                switch (action){
+                    case ConnectivityManager.CONNECTIVITY_ACTION:
+                        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                        if (networkInfo != null && networkInfo.isConnected()) {
+                            // Network connected
+                            Log.d("BroadcastReceiver", "网络连接");
+                            im_wifi.setVisibility(View.VISIBLE);
+                        } else {
+                            // Network disconnected
+                            Log.d("BroadcastReceiver", "网络未连接");
+                            im_wifi.setVisibility(View.GONE);
+                        }
+                        break;
+                    case UsbManager.ACTION_USB_DEVICE_ATTACHED:
+                        // USB device attached
+                        Log.d("BroadcastReceiver", "USB d插入");
+                        im_usb.setVisibility(View.VISIBLE);
+                        break;
+
+                    case UsbManager.ACTION_USB_DEVICE_DETACHED:
+                        // USB device detached
+                        Log.d("BroadcastReceiver", "USB 拔出");
+                        im_usb.setVisibility(View.GONE);
+                        break;
+                    case Intent.ACTION_HEADSET_PLUG:
+                        // Headset plugged or unplugged
+                        int state = intent.getIntExtra("state", -1);
+                        if (state == 0) {
+                            // Headset unplugged
+                            Log.d("BroadcastReceiver", "耳机插入");
+                            im_headset.setVisibility(View.VISIBLE);
+                        } else if (state == 1) {
+                            // Headset plugged in
+                            Log.d("BroadcastReceiver", "耳机拔出");
+                            im_headset.setVisibility(View.GONE);
+                        }
+                        break;
+                }
+            }
+        }
+    };
+
+    private boolean usbIsConnect(){
+        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
+        for (Map.Entry<String,UsbDevice> entry:deviceList.entrySet()){
+            UsbDevice device = entry.getValue();
+            //检测设备是否已经挂载到USB设备
+            return usbManager.hasPermission(device);
+        }
+        return false;
+    }
+
+    private boolean headsetIsWired(){
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        return audioManager.isWiredHeadsetOn();
+    }
 }
